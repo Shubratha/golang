f8565820438d0fe39e115632c707f88f540b9a73
Shubratha <shubratha.leo@gmail.com>
2020-04-12 20:56:02 +0530

    create server

---
+++ tcp/readandwrite.go
@@ -0,0 +1,41 @@
+package main
+import (
+	"fmt"
+	"log"
+	"net"
+	"bufio"
+	"time"
+)
+
+func main() {
+	li, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Panic(err)
+	}
+	defer li.Close()
+
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Println(err)
+		}
+		go handle(conn)
+	}
+}
+
+func handle(conn net.Conn) {
+	err := conn.SetDeadline(time.Now().Add(10 * time.Second)) //set conn timeout
+	if err != nil {
+		log.Println("Connection timeout")
+	}
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fmt.Println(ln)
+		fmt.Fprintf(conn, "I head you say: %s\n", ln)
+
+		defer conn.Close()
+	}
+
+	fmt.Println("The End")
+}
